637b80f1a3f25129f0c2c17415fa4dcf47cfab11
Jatin Kumar <jatinkumar98912@gmail.com>
2026-02-03 15:56:33 +0530

    Add files via upload

---
+++ myname.java
@@ -0,0 +1,9 @@
+public class myname {
+    public static void main(String[] args) {
+        int i;
+        for( i=0; i<=0 && i>-2; i--){
+            System.out.println("m");
+        }
+        
+    }
+}
